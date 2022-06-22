72198c66a412ded3965488f206444c35c3c6cbdc
MoeinT <moin.torabi@gmail.com>
2022-06-22 18:44:39 +0200

    modified the db module

--- tf/database/variables.tf
+++ tf/database/variables.tf
@@ -1,53 +1,30 @@
 variable "DB_name" {
-  type = string
-}
-
-variable "DB_data_retention_time_in_days" {
-  type    = number
-  default = 1
-}
-
-variable "DB_privilege" {
-  type    = string
-  default = "USAGE"
-}
-
-variable "DB_with_grant_option" {
-  type    = bool
-  default = false
+   type  = string
 }
 
 variable "DB_grant_roles" {
-  type = map(any)
+   type  = map(any)
 }
 
-# variable "schema_names" {
-#   type = list(string)
-# }
-
-# variable "SCHEMA_grant_roles" {
-#   type = map(any)
-# }
-
-# variable "schema_privilege" {
-#   type    = string
-#   default = "USAGE"
-# }
-
-variable "schema_with_grant_option" {
-  type    = bool
-  default = false
+variable "DB_data_retention_time_in_days" {
+   type = number
+   default = 1
 }
 
-variable "schema_data_retention_days" {
-  type    = number
-  default = 1
-}
-
-variable "schemas" {
-  type = list(string)
+variable "schema_names" {
+   type = list(string)
 }
 
 variable "schema_grants" {
-  type = map(any)
+   type = map(any)
+}
+
+variable "SCHEMA_data_retention_days" {
+   type = number
+   default = 1 
+}
+
+variable "SCHEMA_with_grant_option" {
+   type = bool
+   default = false
 }
